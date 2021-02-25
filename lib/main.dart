c565ee1e7227dfa8ebb319d5e296600d462118b5
Pri Anton Subardio <prianthon@MacBook-Air-Pri.local>
2021-02-25 20:42:49 +0700

    button

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new application());
+}
+
+class application extends StatefulWidget {
+  @override
+  _applicationState createState() => _applicationState();
+}
+
+class _applicationState extends State<application> {
+
+  String textButton = '';
+
+  void method1(value) {
+    setState(() {
+      textButton = value;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Dummy Application',
+      home: new Scaffold(
+        appBar: new AppBar(
+          backgroundColor: Colors.green,
+          title: new Text('Button Widget'),
+        ),
+        body: new Column(
+          children: <Widget>[
+            new RaisedButton(onPressed: () {
+              method1('You Press Button');
+            },
+            child: new Text('Klik Button'),),
+            new FlatButton(onPressed: null, child: new Text('The Flat Button')),
+            new Text(textButton)
+          ],
+        ),
+      ),
+    );
+  }
+}
