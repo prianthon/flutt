205751bbe3e24220d7907f467bbd61de3cab5e90
Pri Anton Subardio <prianthon@MacBook-Air-Pri.local>
2021-02-27 09:39:05 +0700

    drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MaterialApp(home: new application()));
+  runApp(new MaterialApp(home: application()));
 }
 
 class application extends StatefulWidget {
@@ -10,27 +10,27 @@ class application extends StatefulWidget {
 }
 
 class _applicationState extends State<application> {
-
-  bool sbool = false;
-
   @override
   Widget build(BuildContext context) {
-
-    return new MaterialApp(
-      title: 'Dummy Application',
-      home: new Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: new Text('Switch Widget'),
-        ),
-        body: new Center(
-          child: new Switch(value: sbool, onChanged: (bool sb) {
-            setState(() {
-              sbool = sb;
-              print(sbool);
-            });
-          },
-          ),
+    return new Scaffold(
+      appBar: AppBar(
+        title: new Text('Navigasi Drawer'),
+        backgroundColor: Colors.green,
+      ),
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(accountName: Text('Yusmi Putra'), accountEmail: Text('daput@udacoding.com'),
+            currentAccountPicture: new CircleAvatar(backgroundColor: Colors.black26, child: new Text('P'),),
+            decoration: new BoxDecoration(color: Colors.lightBlue),
+            otherAccountsPictures: <Widget>[
+              new CircleAvatar(backgroundColor: Colors.black26, child: new Text('Y'),),
+              new CircleAvatar(backgroundColor: Colors.black26, child: new Text('W'),),
+            ],),
+            new ListTile(title: new Text('Home Page'), trailing: new Icon(Icons.home),),
+            new ListTile(title: new Text('List Laptop'), trailing: new Icon(Icons.laptop),),
+            new ListTile(title: new Text('Close'), trailing: new Icon(Icons.close),onTap: (){Navigator.pop(context);},),
+          ],
         ),
       ),
     );
